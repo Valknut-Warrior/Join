a58df6cca619e0d503a29bfd414a47a560b753ca
Valknut-Warrior <dev-lemmert@posteo.de>
2024-08-24 19:51:16 +0200

    Summary

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
 "use strict";
 
+const loadingIndicator = document.getElementById("loading");
+
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    showLoading();
 
     themeToggle();
+
+    hideLoading();
 
 });
 
 
+
+/**
+ * Show Spinner
+ */
+function showLoading() {
+    loadingIndicator.classList.add("active");
+}
+
+
+/**
+ * Hide Spinner
+ */
+function hideLoading() {
+    loadingIndicator.classList.remove("active");
+}
