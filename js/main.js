fe0f6a8fb04f2af2461aec87faeaf3df58da3dcf
Valknut-Warrior <dev-lemmert@posteo.de>
2024-09-04 19:37:58 +0200

    Update Login

--- js/main.js
+++ js/main.js
@@ -1,17 +1,40 @@
 "use strict";
 
 const loadingIndicator = document.getElementById("loading");
+const mailInput = document.getElementById("mailInput");
+const pwdInput = document.getElementById("pwdInput");
+const loginPage = document.getElementById("login-button");
 
 
 
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', (event) => {
     showLoading();
     themeToggle();
     hideLoading();
     w3.includeHTML();
-
 });
+
+
+
+mailInput.addEventListener("click", (event) => {
+    mailInput.style.border = "solid 1px var(--border-input-focus)";
+    pwdInput.style.border = "solid 1px var(--border-inputfeld-login";
+});
+
+
+pwdInput.addEventListener("click", (event) => {
+   pwdInput.style.border = "solid 1px var(--border-input-focus)";
+   mailInput.style.border = "solid 1px var(--border-inputfeld-login)";
+});
+
+
+loginPage.addEventListener('click', (event) => {
+    mailInput.style.border = "solid 1px var(--border-inputfeld-login)";
+    pwdInput.style.border = "solid 1px var(--border-inputfeld-login";
+});
+
+
 
 
 
