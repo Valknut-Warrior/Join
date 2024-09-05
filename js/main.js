bbc135f24580a1fe8299d6413ea1a80938bf3f7d
Valknut-Warrior <dev-lemmert@posteo.de>
2024-09-05 19:06:50 +0200

    Summary

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 const loadingIndicator = document.getElementById("loading");
 const mailInput = document.getElementById("mailInput");
 const pwdInput = document.getElementById("pwdInput");
-const loginPage = document.getElementById("login-button");
-
+const signUpButton = document.getElementById("sign-up");
+const loginButton = document.getElementById("log");
 
 
 
@@ -17,22 +17,44 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
 
+// Beim Klicken auf das E-Mail-Feld den Rahmen setzen
 mailInput.addEventListener("click", (event) => {
     mailInput.style.border = "solid 1px var(--border-input-focus)";
-    pwdInput.style.border = "solid 1px var(--border-inputfeld-login";
+    pwdInput.style.border = "solid 1px var(--border-inputfeld-login)";
 });
 
-
+// Beim Klicken auf das Passwort-Feld den Rahmen setzen
 pwdInput.addEventListener("click", (event) => {
-   pwdInput.style.border = "solid 1px var(--border-input-focus)";
-   mailInput.style.border = "solid 1px var(--border-inputfeld-login)";
-});
-
-
-loginPage.addEventListener('click', (event) => {
+    pwdInput.style.border = "solid 1px var(--border-input-focus)";
     mailInput.style.border = "solid 1px var(--border-inputfeld-login)";
-    pwdInput.style.border = "solid 1px var(--border-inputfeld-login";
 });
+
+// Event, das das Entfernen der Rahmen steuert, wenn außerhalb der Felder geklickt wird
+document.addEventListener("click", (event) => {
+    // Prüfen, ob der Klick nicht auf das E-Mail- oder Passwortfeld erfolgt ist
+    if (!mailInput.contains(event.target) && !pwdInput.contains(event.target)) {
+        mailInput.style.border = "solid 1px var(--border-inputfeld-login)"; // Standart Rahmen hinzufügen
+        pwdInput.style.border = "solid 1px var(--border-inputfeld-login)";  // Standart Rahmen hinzufügen
+    }
+});
+
+
+signUpButton.addEventListener("mouseover", (event) => {
+   signUpButton.classList.add("focus-sign-up");
+   signUpButton.classList.remove("sign-up");
+});
+
+signUpButton.addEventListener("mouseout", (event) => {
+    signUpButton.classList.add("sign-up");         // Hinzufügen der ursprünglichen Klasse
+    signUpButton.classList.remove("focus-sign-up"); // Entfernen der Fokus-Klasse
+});
+
+
+loginButton.addEventListener("mouseover", (event) => {
+    loginButton.classList.add("focus-login");
+    loginButton.classList.remove("submit-login");
+});
+
 
 
 
