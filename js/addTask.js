e7fe7d5c7f3760f068f8ad37fa7eaf4aae2d70b8
Valknut-Warrior <164926390+Valknut-Warrior@users.noreply.github.com>
2024-10-26 14:48:33 +0200

    Summary

--- js/addTask.js
+++ js/addTask.js
@@ -1,6 +1,58 @@
 "use strict";
 
+const inputTitle = document.getElementById("title");
+const inputDescription = document.getElementById("description");
+const inputDate = document.getElementById("date");
+const inputCategory = document.getElementById("category");
+const inputSub = document.getElementById("subtask");
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    w3.includeHTML();
+   // w3.includeHTML();
+
+});
+
+
+// Beim Klicken auf das Title-Feld den Rahmen setzen
+inputTitle.addEventListener("click", (event) => {
+    inputTitle.style.border = "solid 1px var(--border-input-focus)";
+    inputDescription.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputDate.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputCategory.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputSub.style.border = "solid 1px var(--border-inputfeld-login)";
+});
+
+// Beim Klicken auf das Description-Feld den Rahmen setzen
+inputDescription.addEventListener("click", (event) => {
+    inputDescription.style.border = "solid 1px var(--border-input-focus)";
+    inputTitle.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputDate.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputCategory.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputSub.style.border = "solid 1px var(--border-inputfeld-login)";
+});
+
+// Beim Klicken auf das Date-Feld den Rahmen setzen
+inputDate.addEventListener("click", (event) => {
+    inputDate.style.border = "solid 1px var(--border-input-focus)";
+    inputTitle.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputDescription.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputCategory.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputSub.style.border = "solid 1px var(--border-inputfeld-login)";
+});
+
+// Beim Klicken auf das Category-Feld den Rahmen setzen
+inputCategory.addEventListener("click", (event) => {
+    inputCategory.style.border = "solid 1px var(--border-input-focus)";
+    inputTitle.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputDescription.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputDate.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputSub.style.border = "solid 1px var(--border-inputfeld-login)";
+});
+
+// Beim Klicken auf das Subtask-Feld den Rahmen setzen
+inputSub.addEventListener("click", (event) => {
+    inputSub.style.border = "solid 1px var(--border-input-focus)";
+    inputTitle.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputDescription.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputDate.style.border = "solid 1px var(--border-inputfeld-login)";
+    inputCategory.style.border = "solid 1px var(--border-inputfeld-login)";
 });
