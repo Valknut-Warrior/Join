d6f555bdd63db51e65c32f0dc1181df8bfdbaa4d
Valknut-Warrior <164926390+Valknut-Warrior@users.noreply.github.com>
2024-11-05 18:36:29 +0100

    Add-Task Summary

--- js/addTask.js
+++ js/addTask.js
@@ -5,11 +5,26 @@ const inputDescription = document.getElementById("description");
 const inputDate = document.getElementById("date");
 const inputCategory = document.getElementById("category");
 const inputSub = document.getElementById("subtask");
+const buttonLow = document.getElementById("prioritize-button-low");
+const buttonMedium = document.getElementById("prioritize-button-medium");
+const buttonHigh = document.getElementById("prioritize-button-high");
+
+const svgLow = document.getElementById("svg-low");
+const svgMedium = document.getElementById("svg-medium");
+const svgHigh = document.getElementById("svg-high");
 
 document.addEventListener("DOMContentLoaded", (event) => {
-   // w3.includeHTML();
+
+    setStartColorSVG();
 
 });
+
+function setStartColorSVG() {
+    svgLow.classList.add("filter-green");
+    svgMedium.classList.add("filter-black");
+    buttonMedium.classList.add("bco","text-color-reverse", "font-bold");
+    svgHigh.classList.add("filter-red");
+}
 
 
 // Beim Klicken auf das Title-Feld den Rahmen setzen
@@ -55,4 +70,56 @@ inputSub.addEventListener("click", (event) => {
     inputDescription.style.border = "solid 1px var(--border-inputfeld-login)";
     inputDate.style.border = "solid 1px var(--border-inputfeld-login)";
     inputCategory.style.border = "solid 1px var(--border-inputfeld-login)";
+});
+
+
+// Add-Task Button Low/Medium/High
+buttonLow.addEventListener("mouseover", (event) => {
+    buttonLow.classList.add("box-shadow");
+    buttonMedium.classList.remove("box-shadow");
+    buttonHigh.classList.remove("box-shadow");
+});
+
+buttonMedium.addEventListener("mouseover", (event) => {
+    buttonMedium.classList.add("box-shadow");
+    buttonLow.classList.remove("box-shadow");
+    buttonHigh.classList.remove("box-shadow");
+});
+
+buttonHigh.addEventListener("mouseover", (event) => {
+    buttonHigh.classList.add("box-shadow");
+    buttonLow.classList.remove("box-shadow");
+    buttonMedium.classList.remove("box-shadow");
+});
+
+// Schatten entfernen, wenn Maus den Button verlässt
+buttonLow.addEventListener("mouseout", () => {
+    buttonLow.classList.remove("box-shadow");
+});
+
+buttonMedium.addEventListener("mouseout", () => {
+    buttonMedium.classList.remove("box-shadow");
+});
+
+buttonHigh.addEventListener("mouseout", () => {
+    buttonHigh.classList.remove("box-shadow");
+});
+
+// Füge `click`-Event-Listener hinzu, um die Farbe beim Klicken zu ändern
+buttonLow.addEventListener("click", () => {
+    buttonLow.classList.add("active-color");
+    buttonMedium.classList.remove("active-color");
+    buttonHigh.classList.remove("active-color");
+});
+
+buttonMedium.addEventListener("click", () => {
+    buttonMedium.classList.add("active-color");
+    buttonLow.classList.remove("active-color");
+    buttonHigh.classList.remove("active-color");
+});
+
+buttonHigh.addEventListener("click", () => {
+    buttonHigh.classList.add("active-color");
+    buttonLow.classList.remove("active-color");
+    buttonMedium.classList.remove("active-color");
 });
